b78f0e31924ba7c2dae3cddff98bd1780a7be903
Leonardo S. Ugiete <leonardougiete@hotmail.com>
2022-02-24 14:27:36 -0300

    Add routes and bloc providers

--- app/lib/app/app.dart
+++ app/lib/app/app.dart
@@ -1,17 +1,29 @@
-import 'package:app/components/pages/settings/settings.dart';
+import 'package:app/bloc/conn/conn_bloc.dart';
+import 'package:app/components/pages/home/home.dart';
+import 'package:app/models/conn.dart';
+import 'package:app/resources/routes.dart';
 import 'package:app/resources/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DockerManagerApp extends StatelessWidget {
   const DockerManagerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: lightTheme,
-      title: 'Docker Manager',
-      debugShowCheckedModeBanner: false,
-      home: const SettingsPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ConnectionBloc>(
+            create: (BuildContext context) =>
+                ConnectionBloc(Connection.local()))
+      ],
+      child: MaterialApp(
+        theme: lightTheme,
+        title: 'Docker Manager',
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: onGenerateRoute,
+        initialRoute: HomePage.route,
+      ),
     );
   }
 }

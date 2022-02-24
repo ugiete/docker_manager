76f9e13ddc2bf04bc992de4111e9a99bde58338d
Leonardo S. Ugiete <leonardougiete@hotmail.com>
2022-02-24 14:26:00 -0300

    Addd route gennerate callback

---
+++ app/lib/resources/routes.dart
@@ -0,0 +1,37 @@
+import 'package:app/components/pages/home/edit-service/edit_service.dart';
+import 'package:app/components/pages/home/home.dart';
+import 'package:app/components/pages/not-found/not-found.dart';
+import 'package:app/components/pages/settings/settings.dart';
+import 'package:app/models/service.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  Widget page = const NotFoundPage();
+
+  if (settings.name == EditServicePage.editRoute) {}
+
+  switch (settings.name) {
+    case HomePage.route:
+      page = const HomePage();
+      break;
+
+    case EditServicePage.editRoute:
+      page = EditServicePage(service: settings.arguments as ServiceModel);
+      break;
+
+    case EditServicePage.newRoute:
+      page = EditServicePage.newService();
+      break;
+
+    case SettingsPage.route:
+      page = SettingsPage();
+      break;
+
+    default:
+      break;
+  }
+
+  return PageRouteBuilder(
+    pageBuilder: (_, __, ___) => page,
+  );
+}

1e92e4dd6c1329571f613cc31ee9c6ec510ca3b0
Leonardo S. Ugiete <leonardougiete@hotmail.com>
2022-02-24 14:28:05 -0300

    Refine default layout

--- app/lib/components/layouts/default.dart
+++ app/lib/components/layouts/default.dart
@@ -1,33 +1,57 @@
+import 'package:app/components/pages/home/home.dart';
+import 'package:app/components/pages/settings/settings.dart';
 import 'package:app/components/ui/bottom.dart';
 import 'package:app/components/ui/header.dart';
 import 'package:app/components/ui/side_menu.dart';
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
-  final Widget body;
-  final Widget? floatButton;
+  final String title;
+  final body;
+  final bool backPath;
+  final int currentMenu;
+  final action;
+  final floatButton;
 
-  const DefaultLayout({required this.body, this.floatButton, Key? key})
+  DefaultLayout(
+      {required this.title,
+      required this.body,
+      required this.currentMenu,
+      this.backPath = false,
+      this.action,
+      this.floatButton,
+      Key? key})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  build(BuildContext context) {
     return Scaffold(
       appBar: const AppHeader(),
       body: Row(
         children: [
-          const AppSideBarMenu(
-            data: [MenuData(Icons.settings, 'settings-path', true)],
+          AppSideBarMenu(
+            data: [
+              MenuData(
+                  Icons.account_tree_rounded, HomePage.route, currentMenu == 0),
+              MenuData(Icons.settings, SettingsPage.route, currentMenu == 1),
+            ],
           ),
           Expanded(
               child: Container(
                   padding: const EdgeInsets.all(20.0),
                   height: double.infinity,
-                  child: SingleChildScrollView(child: body)))
+                  child: SingleChildScrollView(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      PageHeader(
+                          title: title, action: action, backPath: backPath),
+                      body,
+                    ],
+                  ))))
         ],
       ),
-      bottomNavigationBar: AppBottom(
-          data: InfoData('127.0.0.1', true, 0, false), loading: false),
+      bottomNavigationBar: AppBottom(data: InfoData(0, false)),
       floatingActionButton: floatButton,
     );
   }

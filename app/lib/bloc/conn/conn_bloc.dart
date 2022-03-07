a7a6b66136e3315be5919f53bb65166d7a79f479
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 12:53:10 -0300

    Rename state to event

--- app/lib/bloc/conn/conn_bloc.dart
+++ app/lib/bloc/conn/conn_bloc.dart
@@ -1,13 +1,13 @@
-import 'package:app/bloc/conn/conn_state.dart';
+import 'package:app/bloc/conn/conn_events.dart';
 import 'package:app/models/conn.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ConnectionBloc extends Bloc<ConnectionEventBloc, Connection> {
   ConnectionBloc(Connection initialState) : super(initialState) {
     on<ConnectionServiceDown>(
-        (event, emit) => emit(Connection.from(state, newActive: true)));
-    on<ConnectionServiceUp>(
         (event, emit) => emit(Connection.from(state, newActive: false)));
+    on<ConnectionServiceUp>(
+        (event, emit) => emit(Connection.from(state, newActive: true)));
     on<ConnectionSetHost>(
         (event, emit) => emit(Connection.from(state, newHost: event.host)));
     on<ConnectionSetUsername>((event, emit) =>

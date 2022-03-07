6608aac914ec7884065e60a4ed545362591b191e
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 12:54:39 -0300

    Add service bloc to home page

--- app/lib/components/pages/home/home.dart
+++ app/lib/components/pages/home/home.dart
@@ -1,16 +1,17 @@
+import 'package:app/bloc/services/services_bloc.dart';
 import 'package:app/components/layouts/default.dart';
 import 'package:app/components/pages/home/edit-service/edit_service.dart';
 import 'package:app/components/pages/home/table.dart';
-import 'package:app/mock/services.dart';
 import 'package:app/models/service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   static const String route = '/';
 
-  void navigateToEdit(BuildContext context, {ServiceModel? service}) {
+  void navigateToEdit(BuildContext context, {Service? service}) {
     if (service == null) {
       Navigator.of(context).pushNamed(EditServicePage.newRoute);
     } else {
@@ -33,14 +34,17 @@ class HomePage extends StatelessWidget {
             children: [
               SizedBox(
                 width: double.infinity,
-                child: ContainersTable(columns: const [
-                  'Container ID',
-                  'Names',
-                  'Ports',
-                  'Status',
-                  'Created At',
-                  ''
-                ], rows: services),
+                child: BlocBuilder<ServicesBloc, ServiceState>(
+                    builder: (BuildContext context, ServiceState state) {
+                  return ContainersTable(columns: const [
+                    'Container ID',
+                    'Names',
+                    'Ports',
+                    'Status',
+                    'Created At',
+                    ''
+                  ], rows: state.services, isLoading: state.loading);
+                }),
               )
             ]));
   }

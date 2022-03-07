f326aeb83db9d7d96f5368bf7de24688c6a654c1
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 12:57:22 -0300

    Add service BloC

--- app/lib/components/ui/bottom.dart
+++ app/lib/components/ui/bottom.dart
@@ -1,5 +1,7 @@
 import 'package:app/bloc/conn/conn_bloc.dart';
+import 'package:app/bloc/services/services_bloc.dart';
 import 'package:app/models/conn.dart';
+import 'package:app/models/service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,16 +13,17 @@ class InfoData {
 }
 
 class AppBottom extends StatelessWidget {
-  final InfoData data;
-
-  const AppBottom({required this.data, Key? key}) : super(key: key);
+  const AppBottom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        //cond ? const LinearProgressIndicator() : Container(),
+        BlocBuilder<ServicesBloc, ServiceState>(
+            builder: (BuildContext context, ServiceState state) {
+          return state.loading ? const LinearProgressIndicator() : Container();
+        }),
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 10.0),
           color:
@@ -28,10 +31,7 @@ class AppBottom extends StatelessWidget {
           height: 24.0,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const HostStatus(),
-              ServicesStatus(data.servicesRunning, data.hasNotifications)
-            ],
+            children: const [HostStatus(), ServicesStatus()],
           ),
         ),
       ],
@@ -49,11 +49,15 @@ class HostStatus extends StatelessWidget {
       return Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Tooltip(
-              message: state.active ? 'Serviço Ativo' : 'Serviço Parado',
-              child: Icon(Icons.circle,
-                  size: 14.0,
-                  color: state.active ? Colors.greenAccent : Colors.redAccent)),
+          state.active != null
+              ? Tooltip(
+                  message: state.active! ? 'Serviço Ativo' : 'Serviço Parado',
+                  child: Icon(Icons.circle,
+                      size: 14.0,
+                      color: state.active!
+                          ? Colors.greenAccent
+                          : Colors.redAccent))
+              : const Icon(Icons.circle, size: 14.0, color: Colors.white),
           Padding(
             padding: const EdgeInsets.only(left: 10.0),
             child: Text(state.host,
@@ -66,25 +70,24 @@ class HostStatus extends StatelessWidget {
 }
 
 class ServicesStatus extends StatelessWidget {
-  final int servicesRunning;
-  final bool hasNotifications;
-
-  const ServicesStatus(this.servicesRunning, this.hasNotifications, {Key? key})
-      : super(key: key);
+  const ServicesStatus({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text('Serviços rodando: $servicesRunning'),
-        Padding(
-          padding: const EdgeInsets.only(left: 20.0),
-          child: Icon(
-              hasNotifications ? Icons.notifications : Icons.notifications_none,
-              size: 16.0),
-        )
-      ],
-    );
+    return BlocBuilder<ServicesBloc, ServiceState>(
+        builder: (BuildContext context, ServiceState state) {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Serviços rodando: ${state.services.length}'),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Icon(
+                state.loading ? Icons.notifications : Icons.notifications_none,
+                size: 16.0),
+          )
+        ],
+      );
+    });
   }
 }

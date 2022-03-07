11ee6c3981a6f0950d3a7bcf26b8c78c69a5f699
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 12:59:32 -0300

    Add Template route

--- app/lib/resources/routes.dart
+++ app/lib/resources/routes.dart
@@ -2,6 +2,7 @@ import 'package:app/components/pages/home/edit-service/edit_service.dart';
 import 'package:app/components/pages/home/home.dart';
 import 'package:app/components/pages/not-found/not-found.dart';
 import 'package:app/components/pages/settings/settings.dart';
+import 'package:app/components/pages/templates/templates.dart';
 import 'package:app/models/service.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ Route<dynamic>? onGenerateRoute(RouteSettings settings) {
       break;
 
     case EditServicePage.editRoute:
-      page = EditServicePage(service: settings.arguments as ServiceModel);
+      page = EditServicePage(service: settings.arguments as Service);
       break;
 
     case EditServicePage.newRoute:
@@ -25,6 +26,10 @@ Route<dynamic>? onGenerateRoute(RouteSettings settings) {
 
     case SettingsPage.route:
       page = SettingsPage();
+      break;
+
+    case TemplatesPage.route:
+      page = const TemplatesPage();
       break;
 
     default:

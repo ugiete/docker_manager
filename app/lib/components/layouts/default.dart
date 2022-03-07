9af14eefdc76c2230d168ca9cb1677ca0776cda1
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 13:09:38 -0300

    Update default layout

--- app/lib/components/layouts/default.dart
+++ app/lib/components/layouts/default.dart
@@ -1,11 +1,16 @@
+import 'dart:async';
+
 import 'package:app/components/pages/home/home.dart';
 import 'package:app/components/pages/settings/settings.dart';
+import 'package:app/components/pages/templates/templates.dart';
 import 'package:app/components/ui/bottom.dart';
 import 'package:app/components/ui/header.dart';
 import 'package:app/components/ui/side_menu.dart';
+import 'package:app/models/socket.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class DefaultLayout extends StatelessWidget {
+class DefaultLayout extends StatefulWidget {
   final String title;
   final Widget body;
   final bool backPath;
@@ -23,6 +28,20 @@ class DefaultLayout extends StatelessWidget {
       Key? key})
       : super(key: key);
 
+  @override
+  State<DefaultLayout> createState() => _DefaultLayoutState();
+}
+
+class _DefaultLayoutState extends State<DefaultLayout> {
+  late Timer timer;
+  bool loading = true;
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
   void onNavigate(BuildContext context, String path, Object? arguments) {
     Navigator.of(context).pushNamed(path, arguments: arguments);
   }
@@ -37,9 +56,15 @@ class DefaultLayout extends StatelessWidget {
         children: [
           AppSideBarMenu(
             data: [
+              MenuData(Icons.account_tree_rounded, HomePage.route,
+                  widget.currentMenu == 0),
+              MenuData(Icons.file_present_sharp, TemplatesPage.route,
+                  widget.currentMenu == 1),
               MenuData(
-                  Icons.account_tree_rounded, HomePage.route, currentMenu == 0),
-              MenuData(Icons.settings, SettingsPage.route, currentMenu == 1),
+                Icons.settings,
+                SettingsPage.route,
+                widget.currentMenu == 2,
+              ),
             ],
           ),
           Expanded(
@@ -51,14 +76,29 @@ class DefaultLayout extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       PageHeader(
-                          title: title, action: action, backPath: backPath),
-                      body,
+                          title: widget.title,
+                          action: widget.action,
+                          backPath: widget.backPath),
+                      widget.body,
                     ],
                   ))))
         ],
       ),
-      bottomNavigationBar: AppBottom(data: InfoData(0, false)),
-      floatingActionButton: floatButton,
+      bottomNavigationBar: Consumer<ManagerSocket>(
+          builder: (BuildContext context, ManagerSocket state, Widget? child) {
+        if (loading) {
+          Future.delayed(const Duration(seconds: 10), () {
+            state.list();
+            loading = false;
+          });
+        }
+        timer = Timer.periodic(const Duration(seconds: 9), ((Timer timer) {
+          // Verify status every 10 seconds
+          state.status();
+        }));
+        return const AppBottom();
+      }),
+      floatingActionButton: widget.floatButton,
     );
   }
 }

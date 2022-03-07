35bd1669fe630efc7b2ceae31d56be3cf932e9fb
Leonardo S. Ugiete <leonardo.ugiete@etpi.com.br>
2022-03-07 12:52:26 -0300

    Add BloC provider

--- app/lib/app/app.dart
+++ app/lib/app/app.dart
@@ -1,10 +1,14 @@
 import 'package:app/bloc/conn/conn_bloc.dart';
+import 'package:app/bloc/services/services_bloc.dart';
 import 'package:app/components/pages/home/home.dart';
 import 'package:app/models/conn.dart';
+import 'package:app/models/service.dart';
+import 'package:app/models/socket.dart';
 import 'package:app/resources/routes.dart';
 import 'package:app/resources/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class DockerManagerApp extends StatelessWidget {
   const DockerManagerApp({Key? key}) : super(key: key);
@@ -15,14 +19,20 @@ class DockerManagerApp extends StatelessWidget {
       providers: [
         BlocProvider<ConnectionBloc>(
             create: (BuildContext context) =>
-                ConnectionBloc(Connection.local()))
+                ConnectionBloc(Connection.local())),
+        BlocProvider<ServicesBloc>(
+            create: (BuildContext context) =>
+                ServicesBloc(ServiceState(const [], loading: true)))
       ],
-      child: MaterialApp(
-        theme: lightTheme,
-        title: 'Docker Manager',
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: onGenerateRoute,
-        initialRoute: HomePage.route,
+      child: Provider(
+        create: (context) => ManagerSocket(context),
+        child: MaterialApp(
+          theme: lightTheme,
+          title: 'Docker Manager',
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: onGenerateRoute,
+          initialRoute: HomePage.route,
+        ),
       ),
     );
   }
